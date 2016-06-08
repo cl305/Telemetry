b2e56144e6e2a447db3a31c67c71b251cf893f04
Cody <codyli332@gmail.com>
2016-06-08 14:31:00 -0400

    Adding project

---
+++ GyroTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GyroTest
+//
+//  Created by Cody Li on 6/8/16.
+//  Copyright © 2016 Cody Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var tele = Telemetry()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
